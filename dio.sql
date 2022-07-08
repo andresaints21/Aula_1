457a4fa11162fa7519326aacb6cb3f16c6d3ef05
André Santos <andrelimasantos21@gmail.com>
2022-07-08 13:38:51 -0300

    Create dio.sql

---
+++ dio.sql
@@ -0,0 +1,10 @@
+CREATE TABLE pessoas (
+    id INT NOT NULL PRIMARY KEY AUTOINCREMENT,
+    nome VARCHAR(30) NOT NULL,
+    nascimento DATE
+
+)
+
+INSERT INTO pessoas (nome,nascimento) VALUE ('André', '1979 11 21')
+INSERT INTO pessoas (nome,nascimento) VALUE ('Luis', '2000 09 10')
+INSERT INTO pessoas (nome,nascimento) VALUE ('Lima', '1995 05 29')
