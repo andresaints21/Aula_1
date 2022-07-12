5f683217482c64ee5986151b172d4847e22e8757
André Santos <andrelimasantos21@gmail.com>
2022-07-11 21:27:02 -0300

    Update dio.sql

--- dio.sql
+++ dio.sql
@@ -21,3 +21,5 @@ SELECT*FROM pessoa ORDER BY nome
 SELECT*FROM pessoa ORDER BY nome DESC
 
 SELECT COUNT(id), genero FROM pessoa GROUP BY genero;
+
+SELECT * FROM videos_canais AS vc JOIN videos AS v ON vc.fk_video = v.id_video JOIN canais AS c ON vc.fk_canal = c.id_canal;
